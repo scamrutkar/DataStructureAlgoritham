4f744a541e50d483516cc94a0d074a85eb69adac
Sagar Amrutkar <scamrutkar@gmail.com>
2019-08-03 19:33:40 +0530

    Updated Morris postorder Traversal

--- DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/Tree/MorrisPostorderTraversal.java
+++ DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/Tree/MorrisPostorderTraversal.java
@@ -4,47 +4,75 @@ public class MorrisPostorderTraversal {
 
 	Node root;
 
-	private void morrisPostorderTraversal() {
+	void reverse(Node from, Node to) // reverse the tree nodes 'from' 'to'.
+	{
+		if (from == to)
+			return;
+		Node x = from, y = from.right, z;
+		while (true) {
+			z = y.right;
+			y.right = x;
+			x = y;
+			y = z;
+			if (x == to)
+				break;
+		}
+	}
 
-		Node pre;
-		Node current = root;
-		while (current != null) {
+	void printReverse(Node from, Node to) // print the reversed tree nodes 'from' -> 'to'.
+	{
+		reverse(from, to);
 
-			if (current.right == null) {
-				System.out.println(current.val);
-				current = current.left;
-			} else {
-
-				pre = current.right;
-
-				while (pre.left != null && pre.left != current)
-					pre = pre.left;
-
-				if (pre.left == null) {
-					pre.left = current;
-					current = current.right;
-				} else {
-					pre.left = null;
-					System.out.println(current.val);
-					current = current.left;
-				}
-
-			}
-
+		Node p = to;
+		while (true) {
+			System.out.print(p.val+" ");
+			if (p == from)
+				break;
+			p = p.right;
 		}
 
+		reverse(to, from);
 	}
+
+	void morrisPostorderTraversal() {
+		
+		Node dump = new Node(100);
+		dump.left = root;
+		Node current = dump, prev;
+		
+		while (current != null) {
+			if (current.left == null) {
+				current = current.right;
+			} else {
+				prev = current.left;
+				while (prev.right != null && prev.right != current)
+					prev = prev.right;
+
+				if (prev.right == null) {
+					prev.right = current;
+					current = current.left;
+				} else {
+					printReverse(current.left, prev); // call print
+					prev.right = null;
+					current = current.right;
+				}
+			}
+		}
+		
+	}
+
 
 	public static void main(String[] args) {
 
 		MorrisPostorderTraversal tree = new MorrisPostorderTraversal();
-		tree.root = new Node(8);
-		tree.root.left = new Node(4);
-		tree.root.right = new Node(14);
-		tree.root.left.left = new Node(2);
-		tree.root.left.right = new Node(6);
-		tree.root.right.left = new Node(12);
-		tree.root.right.right = new Node(16);
+		
+		
+		tree.root = new Node(1);
+		tree.root.right= new Node(2);
+		tree.root.right.right = new Node(5);
+		tree.root.right.left = new Node(3);
+		tree.root.right.left.right = new Node(4);
+		tree.root.right.right.right = new Node(6);
 		tree.morrisPostorderTraversal();
 
 	}
--- DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/Tree/Node.java
+++ DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/Tree/Node.java
@@ -1,36 +1,42 @@
 package com.sagar.javacase.datastructure.Tree;
 
 public class Node {
-	
+
 	Node left;
 	Node right;
 	int val;
+
 	
+
 	public Node(int val) {
 		super();
 		this.left = null;
 		this.right = null;
 		this.val = val;
 	}
-	
+
 	public Node getLeft() {
 		return left;
 	}
+
 	public void setLeft(Node left) {
 		this.left = left;
 	}
+
 	public Node getRight() {
 		return right;
 	}
+
 	public void setRight(Node right) {
 		this.right = right;
 	}
+
 	public int getVal() {
 		return val;
 	}
+
 	public void setVal(int val) {
 		this.val = val;
 	}
-	
 
 }
