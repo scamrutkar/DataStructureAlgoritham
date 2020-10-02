9bfc13e8e6c933ac7e39b0d2797f88aac5772ebd
Sagar Amrutkar <scamrutkar@gmail.com>
2020-10-02 22:35:39 +0530

    Printing coin change problem solution

--- DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/dp/CoinChangeProblem.java
+++ DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/dp/CoinChangeProblem.java
@@ -1,6 +1,8 @@
 package com.sagar.javacase.datastructure.dp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CoinChangeProblem {
 
@@ -12,6 +14,7 @@ public class CoinChangeProblem {
 
         System.out.println("Solution:");
         System.out.println(getNumberOfWays(8, Coins));
+        printNumberOfWaysSolution(8, Coins);
     }
 
     private static int getNumberOfWays(int n, long[] coins) {
@@ -25,5 +28,30 @@ public class CoinChangeProblem {
             }
         }
         return table[n];
+    }
+
+    private static List<List<Integer>> printNumberOfWaysSolution(int total, long[] coins) {
+        List<List<Integer>> list = new ArrayList<>();
+        printNumberOfWaysSolutionUtil(list, new ArrayList<Integer>(), total, coins,0);
+        return list;
+    }
+
+    private static void printNumberOfWaysSolutionUtil(List<List<Integer>> list, List<Integer> result, long total, long[] coins, int start) {
+        if (total == 0) {
+            System.out.println("Combination: "+result);
+            list.add(new ArrayList<>(result));
+            return;
+        }
+
+        if (total < 0)
+            return;
+
+        for ( int i = start; i < coins.length; i++) {
+            if (coins[i] <= total) {
+                result.add((int) coins[i]);
+                printNumberOfWaysSolutionUtil(list, result, total - coins[i], coins, i);
+                result.remove(result.size() - 1);
+            }
+        }
     }
 }
