d26c5217c3ec05b0a67c44fd13765aa167360f5a
Sagar Amrutkar <scamrutkar@gmail.com>
2020-10-02 19:58:10 +0530

    Kadane's Algo

--- DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/dp/MaxContigousSumSubArray.java
+++ DataStrctureAlgoritham/src/com/sagar/javacase/datastructure/dp/MaxContigousSumSubArray.java
@@ -3,18 +3,32 @@ package com.sagar.javacase.datastructure.dp;
 public class MaxContigousSumSubArray {
 
     public static void main(String[] args) {
-        int[] a = {-1};
+        //int[] a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] a = {-1,-2};
         System.out.println("Maximum contiguous sum is " +
                 maxSubArraySum(a));
     }
 
     private static int maxSubArraySum(int[] a) {
-        int maxGlobal = a[0];
+        int maxGlobal = 0;
         int maxCurrent = 0;
+        int bestStart = 0, bestEnd = 0;
+        int currentStart = 0;
         for (int i = 0; i < a.length; i++) {
-            maxCurrent = Math.max(maxCurrent + a[i], a[i]);
-            maxGlobal = Math.max(maxGlobal, maxCurrent);
+            if(maxCurrent <= 0){
+                maxCurrent = a[i];
+                currentStart = i;
+            }else{
+                maxCurrent += a[i];
+            }
+
+            if(maxCurrent > maxGlobal){
+                maxGlobal = maxCurrent;
+                bestStart = currentStart;
+                bestEnd = i + 1;
+            }
         }
+        System.out.println("BeginI: "+bestStart+", EndI: "+bestEnd);
         return maxGlobal;
     }
 }
